2c8d89fd0246381842aaeda2c6f87713733c8b46
Jackson Lacerda <jacksonlacerda2106@gmail.com>
2021-12-28 19:22:33 -0300

    feat: details http request

--- frontend/src/app/pages/details/details.component.ts
+++ frontend/src/app/pages/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PropertiesApiService } from 'src/app/service/properties-api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  public details: any = []
+
+  constructor(private propertiesApiService: PropertiesApiService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.propertiesApiService.propertiesDetails(id).subscribe(
+      res => this.details = res,
+      error => console.log(error)
+    )
   }
 
 }
